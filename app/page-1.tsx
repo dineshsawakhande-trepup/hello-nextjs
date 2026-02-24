d51433be6d3af179aa5686fb249a327132aba8b1
dineshsawakhande-trepup <dinesh@trepup.com>
2026-02-24 19:27:42 +0530

    test ci/cd

---
+++ app/page-1.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main>
+            <h1>Hello Next.js</h1>
+            <p>Welcome to your Next.js application</p>
+        </main>
+    );
+}
